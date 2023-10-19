79abd8c46e6898555812478c2e1de7ad22d963d2
Nayeem Bhuiyan <111002293+Nayeem746@users.noreply.github.com>
2023-10-20 00:44:38 +0600

    Add files via upload

---
+++ CLUB.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPORTS_CLUB
+{
+    public class player
+    {
+        private int id;
+        private string name;
+        private double salary;
+        private DateTime joinngDate;
+
+        public player()
+        {
+
+        }
+        public player(int id, string name, double salary,DateTime joinngDate)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+            this.joinngDate = joinngDate;
+        }
+        public void ShowPlayerInfo()
+        {
+            Console.WriteLine("Player Name       :" + name);
+            Console.WriteLine("Player Id         :" + id);
+            Console.WriteLine("Player Salary     :" + salary);
+            Console.WriteLine("Joining Date      :" + joinngDate);
+;        }
+
+    }
+
+    public class cricketP:player
+    {
+        private int totalRun;
+        private int totalWiket;
+
+        public cricketP() 
+        { 
+        
+        }
+        public cricketP( int id, string name, double salary, DateTime joinngDate, int totalRun, int totalWicket) :base(id, name, salary, joinngDate)
+        { 
+            this.totalRun = totalRun;
+            this.totalWiket = totalWicket;
+        }
+        public void ShowCricketerInfo()
+        {
+            ShowPlayerInfo();
+            Console.WriteLine("Total Run         :" + totalRun);
+            Console.WriteLine("Total Wicket      :" + totalWiket);
+        }
+
+    }
+    public class footballP:player
+    {
+        private int totalGoal;
+        private int totalAssist;
+
+        public footballP() 
+        { 
+        
+        }
+
+        public footballP(int totalGoal, int totalAssist, int id, string name, double salary, DateTime joinngDate) : base(id, name, salary, joinngDate)
+        { 
+            this.totalGoal = totalGoal; 
+            this.totalAssist = totalAssist; 
+        } 
+        public void ShowFootballerInfo()
+        {
+            ShowPlayerInfo();
+            Console.WriteLine("Total Goal        :" + totalGoal);
+            Console.WriteLine("Total Assist      :" + totalAssist);
+        
+        }
+
+    }
+
+
+
+    internal class CLUB
+    {
+        static void Main(string[] args)
+        {
+            cricketP cricketP = new cricketP(75, "Nayeem", 200000.0, new DateTime(2018, 10, 10), 12070, 150);
+            cricketP.ShowCricketerInfo();
+
+            Console.WriteLine();
+
+            footballP footballP = new footballP(700, 250, 10, "Rafi", 600000, new DateTime(2020,10,7));
+           footballP.ShowFootballerInfo();
+
+        }
+    }
+}
---
+++ ITEAM.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPORTS_ITEAM
+{
+    public class SportsItem
+    {
+        private string brand;
+        private double price;
+        private int quantity;
+
+        public SportsItem()
+        {
+            
+        }
+
+        public SportsItem(string brand, double price, int quantity)
+        {
+            this.brand = brand;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public void SetPrice(double price)
+        {
+            this.price = price;
+        }
+
+        public double GetPrice()
+        {
+            return price;
+        }
+
+        public string GetBrand()
+        {
+            return brand;
+        }
+
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("Brand           : " + brand);
+            Console.WriteLine("Price           : " + price);
+            Console.WriteLine("Quantity        : " + quantity);
+        }
+    }
+
+    public class Customer
+    {
+        private string customerName;
+        private int noOfItem;
+        private double totalPay;
+
+        public Customer()
+        {
+          
+        }
+
+        public Customer(string customerName, int noOfItem, double totalPay)
+        {
+            this.customerName = customerName;
+            this.noOfItem = noOfItem;
+            this.totalPay = totalPay;
+        }
+
+        public string GetCustomerName()
+        {
+            return customerName;
+        }
+
+        public int GetNoOfItem()
+        {
+            return noOfItem;
+        }
+
+        public double GetTotalPay()
+        {
+            return totalPay;
+        }
+
+        public void ShowCustomerInfo()
+        {
+            Console.WriteLine("Customer Name   : " + customerName);
+            Console.WriteLine("Number of Items : " + noOfItem);
+            Console.WriteLine("Total Pay       : " + totalPay);
+        }
+    }
+
+   public  class CricketBat : SportsItem
+    {
+        private double height;
+        private double weight;
+
+        public CricketBat()
+        {
+        }
+
+        public CricketBat(string brand, double price, int quantity, double height, double weight) : base(brand, price, quantity)
+        {
+            this.height = height;
+            this.weight = weight;
+        }
+
+        public void ShowItemInfo()
+        {
+            Info();
+            Console.WriteLine("Item Size       :" + height + " & " + weight);
+        }
+    }
+
+    class Football : SportsItem
+    {
+        private double radius;
+
+        public Football()
+        {
+            
+        }
+
+        public Football(string brand, double price, int quantity, double radius) : base(brand, price, quantity)
+        {
+            this.radius = radius;
+        }
+
+        public void ShowItemInfo()
+        {
+            Info();
+            Console.WriteLine("Radius          : " + radius);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer("Nayeem", 5, 2500.0);
+            Console.WriteLine();
+            customer.ShowCustomerInfo();
+
+            CricketBat cricketBat = new CricketBat("Adidas", 500.0, 10, 30.5, 2.5);
+            cricketBat.ShowItemInfo();
+
+            Console.WriteLine();
+
+            Customer customer1 = new Customer("Rafi", 5, 1000);
+            customer1.ShowCustomerInfo();
+            Football football = new Football("Nike", 200.0, 5, 7.0);
+            football.ShowItemInfo();
+
+        }
+    }
+}
